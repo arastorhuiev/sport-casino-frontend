0fef4365ba8aa1e841024b80865177ca3e557f05
Andrii Rastorhuiev <114357806+arastorhuiev@users.noreply.github.com>
2025-06-19 22:38:56 +0300

    add forms package, add base register popup

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,33 @@ const visible = ref(false)
 <template>
   <div class="card flex justify-center">
     <Button label="Show" @click="visible = true" />
-    <Dialog v-model:visible="visible" modal header="Edit Profile" :style="{ width: '25rem' }">
-      <span class="text-surface-500 dark:text-surface-400 block mb-8"
-        >Update your information.</span
-      >
-      <div class="flex items-center gap-4 mb-4">
-        <label for="username" class="font-semibold w-24">Username</label>
-        <InputText id="username" class="flex-auto" autocomplete="off" />
-      </div>
-      <div class="flex items-center gap-4 mb-8">
-        <label for="email" class="font-semibold w-24">Email</label>
-        <InputText id="email" class="flex-auto" autocomplete="off" />
-      </div>
-      <div class="flex justify-end gap-2">
-        <Button type="button" label="Cancel" severity="secondary" @click="visible = false"></Button>
-        <Button type="button" label="Save" @click="visible = false"></Button>
+    <Dialog
+      v-model:visible="visible"
+      modal
+      header="Register"
+      :style="{ width: '25rem' }"
+      :draggable="false"
+    >
+      <div class="flex flex-col gap-2 w-full">
+        <div class="flex flex-col gap-1">
+          <label for="username" class="font-semibold">Username</label>
+          <InputText id="username" class="flex-auto" />
+        </div>
+        <div class="flex flex-col gap-1">
+          <label for="email" class="font-semibold">Email</label>
+          <InputText id="email" class="flex-auto" />
+        </div>
+        <div class="flex flex-col gap-1">
+          <label for="password" class="font-semibold">Password</label>
+          <InputText id="password" class="flex-auto" />
+        </div>
+        <div class="flex flex-col gap-1">
+          <label for="confirm-password" class="font-semibold">Confirm Password</label>
+          <InputText id="confirm-password" class="flex-auto" />
+        </div>
+        <div class="flex justify-end mt-2">
+          <Button type="button" label="Register" @click="visible = false"></Button>
+        </div>
       </div>
     </Dialog>
   </div>
