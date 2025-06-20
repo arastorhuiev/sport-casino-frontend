7de17c9fe6968cfa315adf754f136865906c9fc8
Andrii Rastorhuiev <114357806+arastorhuiev@users.noreply.github.com>
2025-06-20 21:50:13 +0300

    add base forms with validation

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
 <script setup lang="ts">
 import { Button, Dialog, InputText } from 'primevue'
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
+import { zodResolver } from '@primevue/forms/resolvers/zod'
+import { z } from 'zod'
+import { useToast } from 'primevue/usetoast'
+import { Form, FormField } from '@primevue/forms'
+import Message from 'primevue/message'
+
 const text = ref('')
 const visible = ref(false)
+
+const toast = useToast()
+
+const resolver = zodResolver(
+  z.object({
+    username: z.string().min(1, { message: 'Username is required.' }),
+    email: z.string().email({ message: 'Invalid email address.' }),
+    password: z.string().min(1, { message: 'Password is required.' }),
+    confirmPassword: z.string().min(1, { message: 'Confirm Password is required.' }),
+  }),
+)
+
+const onFormSubmit = ({ valid }: any) => {
+  console.log('Form submitted:', valid)
+  if (valid) {
+    toast.add({ severity: 'success', summary: 'Form is submitted.', life: 3000 })
+  }
+}
 </script>
 
 <template>
-  <div class="flex flex-col gap-12">
-    <div class="flex gap-6 flex-wrap">
-      <div
-        class="rounded-border p-4 border border-transparent flex items-center justify-center bg-primary hover:bg-primary-emphasis text-primary-contrast font-medium flex-auto transition-colors"
-      >
-        primary
-      </div>
-      <div
-        class="rounded-border p-4 border border-transparent flex items-center justify-center bg-highlight hover:bg-highlight-emphasis font-medium flex-auto transition-colors"
-      >
-        highlight
-      </div>
-      <div
-        class="rounded-border p-4 border border-surface flex items-center justify-center text-muted-color hover:text-color hover:bg-emphasis font-medium flex-auto transition-colors"
-      >
-        box
-      </div>
-    </div>
-  </div>
-
   <div class="card flex justify-center">
     <Button label="Show" @click="visible = true" />
     <Dialog
@@ -35,27 +39,39 @@ const visible = ref(false)
       :style="{ width: '25rem' }"
       :draggable="false"
     >
-      <div class="flex flex-col gap-2 w-full">
-        <div class="flex flex-col gap-1">
+      <Form :resolver @submit="onFormSubmit" class="flex flex-col gap-2 w-full">
+        <FormField v-slot="$field" name="username" class="w-full flex flex-wrap gap-1">
           <label for="username" class="font-semibold">Username</label>
-          <InputText id="username" class="flex-auto" />
-        </div>
-        <div class="flex flex-col gap-1">
+          <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{
+            $field.error?.message
+          }}</Message>
+          <InputText id="username" class="w-full" />
+        </FormField>
+        <FormField v-slot="$field" name="email" class="w-full flex flex-wrap gap-1">
           <label for="email" class="font-semibold">Email</label>
-          <InputText id="email" class="flex-auto" />
-        </div>
-        <div class="flex flex-col gap-1">
+          <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{
+            $field.error?.message
+          }}</Message>
+          <InputText id="email" class="w-full" />
+        </FormField>
+        <FormField v-slot="$field" name="password" class="w-full flex flex-wrap gap-1">
           <label for="password" class="font-semibold">Password</label>
-          <InputText id="password" class="flex-auto" />
-        </div>
-        <div class="flex flex-col gap-1">
+          <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{
+            $field.error?.message
+          }}</Message>
+          <InputText id="password" class="w-full" />
+        </FormField>
+        <FormField v-slot="$field" name="confirmPassword" class="w-full flex flex-wrap gap-1">
           <label for="confirm-password" class="font-semibold">Confirm Password</label>
-          <InputText id="confirm-password" class="flex-auto" />
-        </div>
+          <Message v-if="$field?.invalid" severity="error" size="small" variant="simple">{{
+            $field.error?.message
+          }}</Message>
+          <InputText id="confirm-password" class="w-full" />
+        </FormField>
         <div class="flex justify-end mt-2">
-          <Button type="button" label="Register" @click="visible = false"></Button>
+          <Button type="submit" label="Register"></Button>
         </div>
-      </div>
+      </Form>
     </Dialog>
   </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ import router from './router'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
 import App from './App.vue'
+import { ToastService } from 'primevue'
 
 const app = createApp(App)
-
+app.use(ToastService)
 app.use(PrimeVue, {
   theme: {
     preset: Aura,
